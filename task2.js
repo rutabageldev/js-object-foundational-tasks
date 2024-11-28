fa777930f3651dd5173776fe975e2dcfcafb2ac9
rutabageldev <187210498+rutabageldev@users.noreply.github.com>
2024-11-28 13:14:46 -0500

    Completed task 2

---
+++ task2.js
@@ -0,0 +1,12 @@
+let schedule = {};
+
+console.log(isEmpty(schedule));
+schedule["8:30"] = "get up";
+console.log(isEmpty(schedule));
+
+function isEmpty(obj) {
+    for (let key in obj) {
+        return false;
+    }
+    return true;
+}
